9ea700a4bda4b1574613f3415de1e55c051ff220
Peter Jolley <peterjolley456@gmail.com>
2022-08-03 16:09:33 +0930

    swapped server from routes to QraphQL

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express')
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-
+const {typeDefs,resolvers} =require('./schema')
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 
-app.use(express.urlencoded({ extended: true }));
+});
+
+
+
+
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -14,8 +23,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const startApolloServer = async (typeDefs,resolvers) => {
+  await server.start()
+  server.appyMiddleware({ app })
+}
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+startApolloServer(typeDefs,resolvers)
