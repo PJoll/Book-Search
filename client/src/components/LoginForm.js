0b474323036b77f67f077901e5b8937175a642f4
Peter Jolley <peterjolley456@gmail.com>
2022-08-05 14:40:43 +0930

    Login form now uses LOGIN_USER mutation fuctionality

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,13 +1,18 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import {useMutation} from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  
+  const [login] = useMutation(LOGIN_USER);
+  
+  
   const [validated] = useState(false);
+
   const [showAlert, setShowAlert] = useState(false);
 
   const handleInputChange = (event) => {
@@ -26,15 +31,14 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const {data} = await login({
+        variables: {...userFormData}
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      console.log(data);
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
