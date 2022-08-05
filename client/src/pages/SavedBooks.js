242973b3b42d4de83a16cd2909db0d8cbb55a027
Peter Jolley <peterjolley456@gmail.com>
2022-08-05 11:36:02 +0930

    saved and search books changed to use mutations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
+import {useQuery,useMutation} from '@apollo/react-hooks';
+import { REMOVE_BOOK } from '../utils/mutations';
+import {GET_ME } from '../utils/queries'
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const {loading,data} = useQuery(GET_ME)
+ const userData = data?.me || [];
+ const [removeBook] = useMutation(REMOVE_BOOK);
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
@@ -45,16 +48,11 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+       await removeBook({
+        variables: {bookId: bookId}
+       });
+       removeBookId(bookId)
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
