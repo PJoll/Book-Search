f8baf84049d88cfdd1cd302cdb7a4b919c0d450e
Peter Jolley <peterjolley456@gmail.com>
2022-08-16 12:27:58 +0930

    changes to resolvers to fix errors

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -7,17 +7,14 @@ const resolvers = {
     Query:{
     me:async (parent,args,context) => {
         if (context.user) {
-            const userData = await User.findOne ({_id: context.user._id});
+            const userData = await User.findOne ({_id: context.user._id})
+            .select("-__v -password")
+            .populate('books')
             return userData;
         }
     },
 
-users: async () => {
-    return await User.find().select ('__v -password').populate('savedBooks')
-},
-user: async (parent, { username }) => {
-    return await User.findOne({username}).select('__v -password').populate('savedBooks')
-},
+
     },
     Mutation: {
         login: async (parent,{email,password} ) => {
@@ -33,16 +30,20 @@ user: async (parent, { username }) => {
             return{token,user};
         },
         addUser: async(parent,args) => {
+            try{
             const user = await User.create(args);
             const token = await signToken(user);
 
             return{token,user};
+            } catch(err){
+                console.log(err);
+            }
         },
-        saveBook: async (parent, {bookInfo}, context) => {
+        saveBook: async (parent, args, context) => {
             if (context.user) {
                 const updatedUser = await User.findByIdAndUpdate(
                     {_id: context.user._id},
-                    {$push: {savedBooks: bookInfo}},
+                    {$push: {savedBooks: args.input}},
                     {new: true, runValidators: true}
                 );
                 return updatedUser;
